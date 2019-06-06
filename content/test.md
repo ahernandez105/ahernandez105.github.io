401fc06f28f95db2ffa1d627cba0fe6782c66ecf
anhernandez105 <angelhernandez346@gmail.com>
2019-06-05 19:24:45 -0500

    changed cover page of blog at edited first post

--- content/test.md
+++ content/test.md
@@ -1,12 +1,21 @@
 Title: My first post
 Date: 2017-11-26 10:01
 Modified: 2017-11-27 12:30
-Category: misc
+Category: graphical models
 Tags: first, misc
-Slug: My-first-post
-Authors: Your name
-Summary: What does your post talk about? Write here.
+Slug: MY FIRST POST
+Authors: Angel C. Hernandez
+Summary: HERE IS A SUMMARY OF THIS POST
 
-This is the *first post* from my Pelican blog. **YAY!**
+If you're a working data scientist then you have likely heard of Variational Autoencoders and their interesting applications in data generation. There are a fair amount of posts about VAEs, but most brush over the statistical derivation and focus on the high-level intuition and applications. While the statistical make-up can come off as intimidating, I firmly believe grasping the core mathematical concepts acts as an excellent exercise and is must for any aspiring machine learning researcher. As a result, I will do my best to open up VAEs with an emphasis on the statistical components, while still guiding the qualitative intuition. 
 
-![Write here a good description for people who can't see the image](/images/test.png "a title")
+_Contemporary feedback is vital for lucid blog posts, so comments of mistakes, likes and/or dislikes is greatly appreciated. :)_ 
+# Introduction
+At its core, Variational Autoencoders are 
+
+# Mehthodology and Derivation
+Like all generative models, VAEs are concerned with learning a valid probablity over a given dataset $\mathcal{D}$.  
+$$
+p(x)  = \int dx
+$$
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -5,6 +5,14 @@ from __future__ import unicode_literals
 AUTHOR = 'Angel C. Hernandez'
 SITENAME = "angel's blog"
 SITEURL = ''
+SITESUBTITLE = 'My name is Angel C. Hernandez and I am a graduate student at Carnegie Mellon University focusing my studies in machine learning. I am also a recipient of the Science Mathematics and Research for Transformation (SMART) Fellowship, where upon graduation I will be an engineer for the army adhereing to initiatives in warfare simulation modeling at the TRADOC Analysis Center. I have found machine learning blog posts to be an excellent resource during my academic studies and I hope mine can be as beneficial to you as some of ones I have linked below.' 
+SITEIMAGE = '/images/me.png width=300 height=200'
+ICONS = (
+	('github','https://github.com/ahernandez105'),
+	('instagram','https://www.instagram.com/angel__christopher/'),
+	('linkedin','https://www.linkedin.com/in/angel-c-hernandez/')
+)
+
 
 PATH = 'content'
 
@@ -20,19 +28,18 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (("colah's blog", 'https://colah.github.io/'),
+        ('distill', 'https://distill.pub/'),
+        ("Lil'Log", 'https://lilianweng.github.io/lil-log/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 
 STATIC_PATHS = ['images']
