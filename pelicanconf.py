78339ac6f6729995b29db84b3cdca65b2a90ce7b
anhernandez105 <angelhernandez346@gmail.com>
2019-06-06 12:46:20 -0500

    removed plugin submodule

--- pelicanconf.py
+++ pelicanconf.py
@@ -45,4 +45,4 @@ RELATIVE_URLS = True
 STATIC_PATHS = ['images']
 THEME = 'themes/pelican-alchemy/alchemy'
 PLUGIN_PATHS=['/Users/angelhernandez/GitHub/ach_blog/pelican-plugins']
-PLUGINS = ['render_math']
+PLUGINS = ['render_math','latex']
